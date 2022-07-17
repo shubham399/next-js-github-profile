6996a0146afdf1221dc69b6c0160beb3a708fbc9
Shubham Kumar <hello@shubhkumar.in>
2022-07-17 20:34:30 +0530

    Added index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,67 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
+
 const Home: NextPage = () => {
+  const [githubAvatar, setGitHubAvatar] = useState<null | string>(null);
+  const responseDiv = useRef();
+  const [name, setName] = useState<null | string>(null);
+  const [followers, setFollowers] = useState<null | number>(null);
+  const [following, setFollowing] = useState<null | number>(null);
+  const [error, setError] = useState<null | string>(null);
+  const setUser = (username: string) => {
+    const URI = `https://api.github.com/users/${username}`;
+    fetch(URI)
+      .then(res => res.json())
+      .then(data => {
+        setName(data.name || data.login);
+        setGitHubAvatar(data.avatar_url);
+        setFollowing(data.following);
+        setFollowers(data.followers);
+      })
+      .catch(err => {
+        setError("User not found")
+        console.log(err)
+      }
+      );
+  }
+  useEffect(() => {
+    setUser("shubham399");
+  }, [])
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    if (username) {
+      setUser(username);
+      e.target.elements.username.value = null;
+      e.target.elements.username.placeholder = username;
+    }
+  }
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className='flex justify-center align-middle h-screen bg-slate-50 dark:bg-slate-800 text-black dark:text-white'>
+
+        <form className="m-auto" onSubmit={handleSubmit}>
+          <input className="bg-slate-200 dark:bg-slate-600 text-black dark:text-white p-2 m-2 rounded-md" type="text" name="username" placeholder="GitHub Username" autoComplete='false' />
+          <input className="bg-slate-200 dark:bg-slate-600 text-black dark:text-white rounded-md m-2 p-2" type="submit" value="Submit" />
+        </form>
+        <div className='w-1/2 h-screen flex justify-center align-middle'>
+          <div className='m-auto flex-auto justify-center align-middle max-w-md rounded overflow-hidden shadow-lg bg-white mx-auto dark:bg-slate-700 text-black dark:text-white h-1/2'>
+            {error && <div className='text-red-500'>{error}</div>}
+            <h1 className='capitalize text-xl font-Roboto'>{name}</h1>
+            <div className="group flex items-center rounded">
+              {githubAvatar && <Image src={githubAvatar} width="100vh" alt="github" height="100vh" />}
+            </div>
+            {followers && <p>Followers: {followers}</p>}
+            {following && <p>Followings: {following}</p>}
+          </div>
+        </div>
+        {/* <Image src={(githubAvatar || "/doesnot")} alt="Vercel Logo" width={72} height={16} /> */}
+      </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto", "Regular 400"],
+      },
+    },
+    plugins: [],
+
+  }
 }
